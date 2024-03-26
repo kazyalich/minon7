d79e4d0e0eeef90cb95b8d344a5fcf183c7345b1
kazyalich <162959456+kazyalich@users.noreply.github.com>
2024-03-27 01:31:56 +0600

    Add files via upload

---
+++ minon7.php
@@ -0,0 +1,11 @@
+<?php
+
+function printName() {
+    $name = "Артем";
+    $surname = "Иванов";
+    echo "Имя: $name, Фамилия: $surname";
+}
+
+// Вызов функции для вывода имени и фамилии
+printName();
+
